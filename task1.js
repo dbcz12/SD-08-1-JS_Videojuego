2a5b4d93bd1e6233f8db50f90ba490315e9da970
Diana Beatriz Cruz Zamudio <anaid.120393@gmail.com>
2025-09-05 15:05:22 -0600

    Cambio en videojuegos

--- task1.js
+++ task1.js
@@ -1,7 +1,9 @@
 export class Player {
-    constructor() {
-      
-    }
-  
-    
+      constructor(name) {
+        this.name = name;
+      }
   }
+  const jugador1 = new Player("Ana");
+  const jugador2 = new Player("Carlos");
+  console.log(jugador1.name); // "Ana"
+  console.log(jugador2.name); // "Carlos"
--- task4.js
+++ task4.js
@@ -1,7 +1,21 @@
 export class Player {
-    constructor() {
-      
+    constructor(name, level) {  
+        this.name = name;
+        this.level = level;   
     }
-  
-    
-  }
+
+    info() {
+        console.log(`${this.name} has reached Level ${this.level}!`); 
+    }
+
+    levelUp() {
+        this.level++;
+    }
+
+}  // ← Cerrar la clase
+
+const jugador1 = new Player("Diana", 10);
+const jugador2 = new Player("Mario", 12);
+
+jugador1.info(); // "Diana has reached Level 10!"
+jugador2.info(); // "Mario has reached Level 12!"
