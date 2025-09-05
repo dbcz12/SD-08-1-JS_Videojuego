715925626811b7ead2c46b01bdbb190c3c0b7901
Diana Beatriz Cruz Zamudio <anaid.120393@gmail.com>
2025-09-05 14:43:01 -0600

    Videojuegos practica

--- task2.js
+++ task2.js
@@ -1,7 +1,9 @@
 export class Player {
-    constructor() {
-      
+    constructor(name, level) {
+      this.name = name;
+      this.level = level;
     }
-  
-    
   }
+
+const jugador1 = new Player("Diana", 10);
+const jugador2 = new Player("Mario", 12);
--- task4.js
+++ task4.js
@@ -1,7 +1,21 @@
 export class Player {
-    constructor() {
-      
+    constructor(name, level) {  // ← Corregido: "level" no "lavel"
+        this.name = name;
+        this.level = level;     // ← Ahora funciona porque el parámetro se llama "level"
     }
   
-    
-  }
+    info() {
+        console.log(`${this.name} has reached Level ${this.level}!`); // ← console.log y agregado "!"
+    }
+
+    levelUp() {
+        this.level++;
+    }
+
+}  // ← Cerrar la clase
+
+const jugador1 = new Player("Diana", 10);
+const jugador2 = new Player("Mario", 12);
+
+jugador1.info(); // "Diana has reached Level 10!"
+jugador2.info(); // "Mario has reached Level 12!"
